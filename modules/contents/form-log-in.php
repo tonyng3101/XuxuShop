f950771a75243001427b23a382635a793a642189
tonyng3101 <tonyng3101@users.noreply.github.com>
2018-01-08 11:15:24 +0700

    fix size

--- modules/contents/form-log-in.php
+++ modules/contents/form-log-in.php
@@ -1,19 +1,13 @@
 <div id="login" class="parallax text-center">
 	<h1 style="font-family: 'GroteskBoldCond'; font-weight: 500; font-size: 112px; color: #fff; letter-spacing: 13.5px; padding-top: 100px;"> SIGN IN</h1>
 	<hr  width="10px" color="#fff" style="border:2px solid #fff" />
-	<div class="form-login" style="left: 33%;">
+	<div class="form-login">
       	<form action="log-in.php" method="post" enctype="multipart/form-data">
-        	<div class="col-sm-4 form-group">
           		<input class="form-control" id="username" name="username" placeholder="Username" type="text" required>
-        	</div>
-      		<div class="col-sm-4 form-group" style="padding: 0;">
+        	
           		<input class="form-control" id="password" name="password" placeholder="Password" type="password" required>
-        	</div>
-      		<div class="row">
-        		<div class="col-sm-4 form-group">
-          			<button class="btn btn-default pull-right" style="width: 100%; border-radius: 0;border-color: #B97F6D;  background-color: #B97F6D; color: #fff" type="submit">Log In</button>
-        		</div>
-      		</div> 
+
+          		<button class="btn btn-default pull-right" style="width: 100%; border-radius: 0;border-color: #B97F6D;  background-color: #B97F6D; color: #fff" type="submit">Log In</button>
       </form>
   </div>
   </div>
