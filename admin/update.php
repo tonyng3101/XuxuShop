f2820b0912ded0f4936a9b9552c31d21fe9a5ca0
hoan208 <35210962hoan208@users.noreply.github.com>
2018-01-29 21:02:47 +0700

    a

--- admin/update.php
+++ admin/update.php
@@ -9,14 +9,15 @@
 	$gioithieu = $_POST['txtgioithieu'];
 	$mota = $_POST['txtmota'];
 	$filename = $_FILES['bookimage']['name'];
-	echo $filename;
+	echo $filename;	
 	$status=$_POST['status'];
 
 	
 	$sql = "update san_pham set ten_sp = '{$ten}', gia_sp = {$gia}, giam_gia = '{$giamgia}', gioithieu_sp = '{$gioithieu}', mota_sp = '{$mota}', hinhanh_sp = '{$filename}', status = '{$status}' where id_sp = {$id}";
 	
-	mysql_query($sql);
+	
 	move_uploaded_file($_FILES['bookimage']['tmp_name'], 'img/' . $filename);
+	mysql_query($sql);
 	
 	header('Location: danhsachsanpham.php');
 ?>
